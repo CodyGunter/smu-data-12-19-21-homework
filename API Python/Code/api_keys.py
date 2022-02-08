8b54f9b00518b1f5f82ec065de467ae3da191628
CodyGunter <codyjgunter2@gmail.com>
2022-02-07 19:34:28 -0800

    API Python

--- API Python/Code/api_keys.py
+++ API Python/Code/api_keys.py
@@ -2,4 +2,4 @@
 weather_api_key = "e5da7e6748c53702ac3ed10774e956ee"
 
 # Google API Key
-g_key = "e5da7e6748c53702ac3ed10774e956ee"
+g_key = "AIzaSyCqzbTZaBbXBLKaY3gimMs_RhVfbSyuCng"
--- API Python/starter_code/api_keys_Copy1.py
+++
@@ -1,5 +0,0 @@
-# OpenWeatherMap API Key
-weather_api_key = "AIzaSyCqzbTZaBbXBLKaY3gimMs_RhVfbSyuCng"
-
-# Google API Key
-g_key = "AIzaSyCqzbTZaBbXBLKaY3gimMs_RhVfbSyuCng"
